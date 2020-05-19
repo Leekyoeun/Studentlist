544827bc5ec3c892de3bc23914baf95b37d2f71e
s2018 <s2018s35@e-mirim.hs.kr>
2020-05-19 09:29:43 +0900

    20200519

--- Studentlist/Program.cs
+++ Studentlist/Program.cs
@@ -8,8 +8,27 @@ namespace Studentlist
 {
     class Program
     {
+        class Teacher
+        {
+            public string name;
+            public int rade;
+            public string major;
+        }
         static void Main(string[] args)
         {
+            List<Teacher> list = new List<Teacher>();
+            list.Add(new Teacher() { name = "이교은",major="뉴미디어콘텐츠" });
+            list.Add(new Teacher() { name = "박성래", major = "수학" });
+            list.Add(new Teacher() { name = "민주리", major = "정보컴퓨터" });
+
+            foreach(var item in list)
+            {
+                if (item.major == "정보컴퓨터")
+                {
+                    list.Remove(item);
+                }
+                Console.WriteLine(item.name+":"+item.major);
+            }
         }
     }
 }
---
+++ Studentlist/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Studentlist
+{
+    class Student
+    {
+        public string id;
+        public string nam;
+        public int grade;
+        public string major;
+        public DateTime birthday;
+    }
+}
